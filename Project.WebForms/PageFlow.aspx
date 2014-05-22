8c2eba30f7430796571258f2385eb35f681e4bf9
Oleg Dekhtiarenko <devili4enko@gmail.com>
2014-05-22 17:33:25 +0300

    added pageflow webform

---
+++ Project.WebForms/PageFlow.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PageFlow.aspx.cs" Inherits="Project.WebForms.PageFlow" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label runat="server" ID="TestLable" EnableViewState="false" ></asp:Label>0
+        <asp:Button runat="server" ID="TestBtn" Text="Button" OnClick="TestBtn_Click" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Project.WebForms/PageFlow.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Project.WebForms
+{
+    public partial class PageFlow : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            foreach (Control item in Controls)
+            {
+                Response.Write(item.GetType().ToString() + " " + item.ID + "<br/> ");
+            }
+            Response.Write("<hr/>");
+            TestLable.Text += "PageLoad " + Environment.NewLine;
+
+            if (Page.IsPostBack)
+            {
+                TestLable.Text += "            123                              " + Environment.NewLine;
+            }
+
+        }
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            TestLable.Text += "Page_Init " + Environment.NewLine;
+
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            TestLable.Text += "Page_PreRender " + Environment.NewLine;
+
+        }
+        protected void Page_Unload(object sender, EventArgs e)
+        {
+            TestLable.Text += "Unload " + Environment.NewLine;
+
+        }
+
+        protected void TestBtn_Click(object sender, EventArgs e)
+        {
+            TestLable.Text += "TestBtn_Click " + Environment.NewLine;
+        }
+    }
+}
